e6ced2c03a8c0f9ba2d6f7af0b3df0029f400089
Igor_Ilyin_79 <igor.ilyin.78@gmail.com>
2020-05-13 14:42:34 +0300

    Hello_world_to_browser

--- src/main/java/com/test12052020/test12052020/controller/HelloController.java
+++ src/main/java/com/test12052020/test12052020/controller/HelloController.java
@@ -1,27 +1,11 @@
 package com.test12052020.test12052020.controller;
 
-import com.test12052020.test12052020.SocketTest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Scanner;
-
 @RestController
 public class HelloController {
-    @RequestMapping("/")
-   /* public String getHelloWorld() {
-        public static void main (String[] args) throws IOException {
-            try (Socket s =new Socket("time-a.nist.gov", 13);
-                 Scanner in = new Scanner(s.getInputStream(), "UTF-8")){
-                while ( in.hasNextLine()){
-                    String line = in.nextLine();
-                    //System.out.println(line);
-                }
-            }
-        }*/
-        //return "Hello world";
-        return SocketTest();
-    }
+        @RequestMapping("/")
+        public String getHelloWorld(){
+        return "Hello world";}
 }
