5a8e9cd080f2a63be7565de39d1e4e0627154703
Igor_Ilyin_79 <igor.ilyin.78@gmail.com>
2020-05-13 13:03:04 +0300

    Start

---
+++ src/main/java/com/test12052020/test12052020/Test12052020Application.java
@@ -0,0 +1,13 @@
+package com.test12052020.test12052020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12052020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test12052020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/test12052020/test12052020/Test12052020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test12052020.test12052020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test12052020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
